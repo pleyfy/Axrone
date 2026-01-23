18257a82952d25d463f15e8ecfc31d3b23facfb8
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-01-23 22:20:53 +0300

    Add physics types index file

---
+++ packages/core/src/physics/types/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @module physics/types
+ * @description Physics types and primitives
+ */
+
+export * from './primitives';
+export * from './physics-body';
+export * from './collision';
+export * from './constraints';
+export * from './world';
