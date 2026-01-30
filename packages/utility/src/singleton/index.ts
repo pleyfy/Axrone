2092f530c30038b43958122051fc40fc8e02a67c
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-01-30 09:13:01 +0300

    Add singleton utilities and exports to utility package

---
+++ packages/utility/src/singleton/index.ts
@@ -0,0 +1,39 @@
+export type {
+    ScopeDisposer,
+} from './singleton-core';
+export * from './singleton-core';
+
+export { SingletonImpl } from './singleton-impl';
+export { AsyncSingletonImpl } from './async-singleton-impl';
+export { ScopedSingletonImpl } from './scoped-singleton-impl';
+export { SingletonScopeImpl, createRootScope } from './singleton-scope';
+export {
+    SingletonRegistryImpl,
+    getGlobalRegistry,
+    resetGlobalRegistry,
+    resetGlobalRegistryAsync,
+} from './singleton-registry';
+
+export { SingletonError, SingletonErrorCode } from './singleton-errors';
+
+export {
+    create,
+    createAsync,
+    createScoped,
+    createLazy,
+    createLazyAsync,
+    fromValue,
+    fromPromise,
+    createRegistered,
+    createRegisteredAsync,
+    isSingleton,
+    isAsyncSingleton,
+    isScopedSingleton,
+    isAnySingleton,
+    resolve,
+    tryResolve,
+    map,
+    mapAsync,
+    combine,
+    combineAsync,
+} from './singleton-utils';
