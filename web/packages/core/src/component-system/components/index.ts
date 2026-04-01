65764c2724dd8b2edbff8c92ce11488bbbcd4976
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-04-01 12:54:37 +0300

    refactor: consolidate numeric exports into a single statement

--- web/packages/core/src/component-system/components/index.ts
+++ web/packages/core/src/component-system/components/index.ts
@@ -3,8 +3,6 @@ export { Transform } from './transform';
 import { Transform } from './transform';
 (globalThis as any).Transform = Transform;
 
-export { Mat4 } from '@axrone/numeric/src/mat4';
-export { Vec3 } from '@axrone/numeric/src/vec3';
-export { Quat } from '@axrone/numeric/src/quat';
+export { Mat4, Vec3, Quat } from '@axrone/numeric';
 
 export * from './physics';
