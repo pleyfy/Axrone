74022b418310b571aeabd9bab23df6d165fdcf64
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2026-04-29 13:49:51 +0300

    feat: add lighting package with types and guards

---
+++ web/packages/lighting/src/core.ts
@@ -0,0 +1,12 @@
+export * from './constants';
+export * from './brands';
+export * from './errors';
+export * from './types';
+export * from './guards';
+export {
+    DEFAULT_LIGHTING_CAPACITY,
+    DEFAULT_LIGHTING_ENVIRONMENT,
+    createLightingEnvironment,
+    resolveLightingCapacity,
+} from './validation';
+export { LightingRig } from './rig';
